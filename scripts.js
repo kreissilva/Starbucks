35d4b388ba18a9fd2439cd2d3265197bedfac335
Kauã Reis da Silva <kauareisdasilva13@gmail.com>
2025-08-08 15:39:58 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector('.starbucks')
+let fundo = document.querySelector('.circulo')
+
+function trocarImagem(endereco){
+    imagem.src = endereco
+}
+
+let circulo = document.querySelector('.circulo')
+
+function trocarACor(cor){
+    circulo.style.background = cor
+}
